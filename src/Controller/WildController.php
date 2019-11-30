1c7d37357443b176909f67144f0b6af7d1674be4
guillaume67450 <guillaume_d_mundo@hotmail.com>
2019-11-30 12:13:49 +0100

    ajout slug

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -17,4 +17,52 @@ Class WildController extends AbstractController
                 'website' => 'Wild Séries',
         ]);
     }
+
+    /**
+    * @Route("/wild/show/{slug<^[a-z0-9-]+$>}",
+    *     defaults={"slug"=null},
+    *     name="wild_show"
+    * )
+     * 
+     * slug wcs
+     */
+    public function show($slug) :Response
+    {   
+        return $this->render('wild/show.html.twig',
+        [ 
+        'titre' => $slug
+        ]
+        );
+    }
+
+/*
+    static public function slugify($text)
+    {
+        // replace non letter or digits by -
+        $text = preg_replace('~[^\\pL\d]+~u', '-', $text);
+    
+        // trim
+        $text = trim($text, '-');
+    
+        // transliterate
+        if (function_exists('iconv'))
+        {
+            $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+        }
+    
+        // lowercase
+        $text = strtolower($text);
+    
+        // remove unwanted characters
+        $text = preg_replace('~[^-\w]+~', '', $text);
+    
+        if (empty($text))
+        {
+            return 'n-a';
+        }
+    
+        return $text;
+    }
+*/
+
 }
