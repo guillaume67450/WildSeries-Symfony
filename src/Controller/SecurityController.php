c0572a30b41f4a67f57a218a6f134e3fe545446c
guillaume67450 <guillaume_d_mundo@hotmail.com>
2020-07-21 02:33:08 +0200

    21 inscription utilisateur

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,10 +2,19 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use App\Security\LoginFormAuthenticator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\Security\Core\Security;
 
 class SecurityController extends AbstractController
 {
@@ -32,5 +41,67 @@ class SecurityController extends AbstractController
     public function logout()
     {
         throw new \Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
+    }
+
+    /**
+     * @Route("/register", name="app_register")
+     */
+     public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, LoginFormAuthenticator $authenticator): Response
+     {
+        $user = $this->getUser();
+
+        if($user <> null) {
+            $this->addFlash(
+                'warning',
+                "On ne peut pas accéder à la page d'inscription si on est connecté avec un compte !"
+            );
+
+            return $this->redirectToRoute('app_index');
+        }
+
+         $user = new User();
+         $form = $this->createForm(RegistrationFormType::class, $user);
+         $form->handleRequest($request);
+ 
+         if ($form->isSubmitted() && $form->isValid()) {
+             // encode the plain password
+             $user->setPassword(
+                 $passwordEncoder->encodePassword(
+                     $user,
+                     $form->get('plainPassword')->getData()
+                 )
+             );
+ 
+             $entityManager = $this->getDoctrine()->getManager();
+             $entityManager->persist($user);
+             $entityManager->flush();
+ 
+             // do anything else you need here, like send an email
+ 
+             return $guardHandler->authenticateUserAndHandleSuccess(
+                 $user,
+                 $request,
+                 $authenticator,
+                 'main' // firewall name in security.yaml
+             );
+         }
+ 
+         return $this->render('security/register.html.twig', [
+             'registrationForm' => $form->createView(),
+         ]);
+     }
+
+    /**
+     * Permet d'afficher le profil de l'utilisateur connecté
+     * 
+     * @Route("/my-profile", name="profile")
+     * IsGranted("ROLE_USER")
+     * 
+     * @return Response
+     */    
+    public function myAccount() {
+        return $this->render('security/profile.html.twig', [
+            'user' => $this->getUser()
+        ]);
     }
 }
